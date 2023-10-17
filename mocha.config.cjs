f8e48a4a97238b82b5790264cd0d918cc41e9572
Kristiyan Merdzhov <88944722+NewPhoneWhoDis@users.noreply.github.com>
2023-10-17 08:19:07 +0200

    unit tests with vite

--- mocha.config.cjs
+++ mocha.config.cjs
@@ -1,7 +1,7 @@
 // mocha.config.cjs
 module.exports = {
-  require: ['ts-node/register', 'tsconfig-paths/register'], 
-  extension: ['ts', 'tsx'], 
-  spec: '**/*.test.ts*', 
+  require: ['ts-node/register'],
+  extension: ['ts', 'tsx'],
+  spec: '**/*.test.ts*', // Updated pattern to match both .ts and .tsx test files
   recursive: true,
 };
